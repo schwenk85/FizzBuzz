f807f6c0458c739b165484606867e8529a688527
Jochen <kontakt@jochenschwenk.de>
2017-08-22 00:20:12 +0200

    Commit project

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fizzBuzz;
+
+            for (int number = 1; number <= 100; number++)
+            {
+                fizzBuzz = null;
+
+                if (number % 3 == 0)
+                {
+                    fizzBuzz += "Fizz";
+                }
+                if (number % 5 == 0)
+                {
+                    fizzBuzz += "Buzz";
+                }
+                Console.WriteLine(fizzBuzz ?? number.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
